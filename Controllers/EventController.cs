44ce22c4200cc8320cf00e9b7459ba8e4dee20e1
Mateusz Motkowicz <mma.style113@gmail.com>
2021-05-25 17:08:47 +0200

    Controllers

---
+++ Controllers/EventController.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using EventsOrganizer.Entities;
+using EventsOrganizer.Models;
+using EventsOrganizer.Serveces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsOrganizer.Controllers
+{
+    [ApiController]
+    [Route("api/event")]
+    public class EventController : ControllerBase
+    {
+        private readonly IEventService _service;
+
+        public EventController(IEventService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("admin")]
+        public ActionResult<IEnumerable<GetEventDtoAdmin>> GetAllForAdmin()
+        {
+            var events = _service.GetEventsForAdmin();
+
+            return Ok(events);
+        }
+        
+        [HttpGet("member")]
+        public ActionResult<IEnumerable<GetEventDtoMember>> GetAllForMember()
+        {
+            var events = _service.GetEventsForMember();
+
+            return Ok(events);
+        }
+
+        [HttpPost]
+        public ActionResult Create([FromBody] CreateEventDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var guidEvent = _service.CreateEvent(dto);
+
+            return Created($"api/event/{guidEvent}", null);
+        }
+
+        [HttpDelete("{guid}")]
+        public ActionResult Delete([FromRoute] Guid guid)
+        {
+            var isDeleted = _service.DeleteEvent(guid);
+
+            if (isDeleted)
+                return NoContent();
+           
+            return NotFound();
+        }
+
+    }
+}
---
+++ Controllers/MemberController.cs
@@ -0,0 +1,39 @@
+﻿using EventsOrganizer.Models;
+using EventsOrganizer.Serveces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsOrganizer.Controllers
+{
+    [ApiController]
+    [Route("api/member")]
+    public class MemberController : ControllerBase
+    {
+        private readonly IMemberService _service;
+
+        public MemberController(IMemberService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public ActionResult Create([FromBody] CreateMemberDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var member = _service.CreateMember(dto);
+
+            if (!member)
+                return NotFound("Brak wolnych miejsc lub wydarzenia o podanym identyfikatorze");
+
+            return Created("Użytkownik został dodany do wydarzenia", null);
+        }
+    }
+}
