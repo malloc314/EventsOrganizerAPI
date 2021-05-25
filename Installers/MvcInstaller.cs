61b4b3744fb62a87bccdb72cc182d0103ce0b1d7
Mateusz Motkowicz <mma.style113@gmail.com>
2021-05-25 17:10:02 +0200

    Installers

---
+++ Installers/MvcInstaller.cs
@@ -0,0 +1,30 @@
+﻿using EventsOrganizer.Entities;
+using EventsOrganizer.Serveces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsOrganizer.Installers
+{
+    public class MvcInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<EventOrganizerDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("EventOrganizerCS")));
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddScoped<IEventService, EventService>();
+            services.AddScoped<IMemberService, MemberService>();
+
+            services.AddControllers();
+        }
+    }
+}
---
+++ Installers/SwaggerInstaller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsOrganizer.Installers
+{
+    public class SwaggerInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                //c.EnableAnnotations();
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Events Organizer API", Version = "v1" });
+            });
+        }
+    }
+}
